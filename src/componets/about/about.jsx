4488177609a4dd88323832269c2cbb906e52c8b0
Esteban Hulin <esteban.hulin@epitech.eu>
2025-06-17 12:19:17 +0200

    Fix responsive i

--- src/componets/about/about.jsx
+++ src/componets/about/about.jsx
@@ -11,23 +11,29 @@ export const About = () => {
         <div className={styles.content}>
             <ul className={styles.list}>
                 <li className={styles.item}>
-                    <FaSymfony className={styles.icon} />
-                    <h3 className={styles.subtitle}>Work Experience</h3>
+                    <div className={styles.titleContainer}>
+                        <FaSymfony className={styles.icon} />
+                        <h3 className={styles.subtitle}>Work Experience</h3>
+                    </div>
                     <p className={styles.description}>
                     I got hands-on experience as a PHP Symfony developer during a 6-month internship at 6TM.
                     I worked on cool back-end and front-end features for big clients, making their apps better and smoother.
                     </p>
                 </li>
                 <li className={styles.item}>
-                    <FaSchool className={styles.icon} />
-                    <h3 className={styles.subtitle}>Education Journey</h3>
+                    <div className={styles.titleContainer}>
+                        <FaSchool className={styles.icon} />
+                        <h3 className={styles.subtitle}>Education Journey</h3>
+                    </div>
                     <p className={styles.description}>
                     I spent a year studying at BINUS University in Jakarta, Indonesia, during my 4th year of studies at Epitech, where I graduated with a degree in Computer Science in 2024.
                     </p>
                 </li>
                 <li className={styles.item}>
-                    <FaComputer className={styles.icon} />
-                    <h3 className={styles.subtitle}>What’s Next</h3>
+                    <div className={styles.titleContainer}>
+                        <FaComputer className={styles.icon} />
+                        <h3 className={styles.subtitle}>What’s Next</h3>
+                    </div>
                     <p className={styles.description}>
                     These days, I’m all about creating awesome personal projects and learning new technologies to be a better developer.
                     </p>
